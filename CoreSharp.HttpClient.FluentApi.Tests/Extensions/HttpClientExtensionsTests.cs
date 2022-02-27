c4e105af319de3401e3f49e780d8de58c59e6612
Efthymios <efthymios.ks@gmail.com>
2022-02-27 10:24:16 +0200

    Tests, Added for HttpClientExtensions.

---
+++ CoreSharp.HttpClient.FluentApi.Tests/Extensions/HttpClientExtensionsTests.cs
@@ -0,0 +1,37 @@
+﻿using CoreSharp.HttpClient.FluentApi.Contracts;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using Http = System.Net.Http;
+
+namespace CoreSharp.HttpClient.FluentApi.Extensions.Tests
+{
+    [TestFixture]
+    public class HttpClientExtensionsTests
+    {
+        [Test]
+        public void Request_ClienItNull_ThrowArgumentNullException()
+        {
+            //Arrange
+            Http.HttpClient client = null;
+
+            //Act
+            Action action = () => client.Request();
+
+            //Assert
+            action.Should().ThrowExactly<ArgumentNullException>();
+        }
+
+        [Test]
+        public void Request_WhenCalled_ReturnRequest()
+        {
+            //Act
+            using var client = new Http.HttpClient();
+            var request = client.Request();
+
+            //Assert
+            request.Should().NotBeNull();
+            request.Should().BeAssignableTo<IRequest>();
+        }
+    }
+}
